e566e8abc9db5458bbe6e5a0cc5fb35593a1f7ae
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-12 00:48:53 -0600

    dep: add junit

--- earth-api/build.gradle.kts
+++ earth-api/build.gradle.kts
@@ -4,4 +4,10 @@ plugins {
 
 dependencies {
     compileOnlyApi("org.checkerframework:checker-qual:3.21.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
