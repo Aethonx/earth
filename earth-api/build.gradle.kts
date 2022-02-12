7166aeb875bd1d05f01d46c8ca0d92e9bbb1ca5e
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-12 01:04:21 -0600

    dep: add assertj

--- earth-api/build.gradle.kts
+++ earth-api/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     compileOnlyApi("org.checkerframework:checker-qual:3.21.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
 }
 
 tasks.withType<Test> {
