25536497b915f88927e621fee418771724910919
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-18 22:21:27 -0600

    chore: setup Paper plugin.

---
+++ earth-platform/src/main/java/io/shaded/earth/platform/EarthPlatform.java
@@ -0,0 +1,11 @@
+package io.shaded.earth.platform;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EarthPlatform extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+}
