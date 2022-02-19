92f4208de5510115afb6c318d44a54519eb5261e
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-18 22:19:01 -0600

    dep: add paper to `earth-api`.

--- earth-api/build.gradle.kts
+++ earth-api/build.gradle.kts
@@ -2,7 +2,12 @@ plugins {
     id("earth.java-conventions")
 }
 
+repositories {
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
     compileOnlyApi("org.checkerframework:checker-qual:3.21.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
