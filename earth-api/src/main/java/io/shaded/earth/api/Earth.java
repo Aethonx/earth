a37857c7b020e24c2d174be778055f143c48c93b
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-20 23:04:12 -0600

    chore: add JSR305 annotations on exceptions and add `EarthInUseExecution`.

--- earth-api/src/main/java/io/shaded/earth/api/Earth.java
+++ earth-api/src/main/java/io/shaded/earth/api/Earth.java
@@ -1,43 +1,30 @@
 package io.shaded.earth.api;
 
-import io.shaded.earth.api.world.EarthWorld;
-import java.util.UUID;
-import java.util.function.Consumer;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import io.shaded.earth.api.service.EarthService;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-public interface Earth {
+/**
+ * Provides static access into the core functions of Earth.
+ */
+public final class Earth {
 
-  /**
-   * @param worldId the world that is being attempted to be loaded.
-   * @return the resulting world if loaded.
-   */
-  @NonNull EarthWorld load(final @NonNull UUID worldId);
+  @Inject
+  private static @MonotonicNonNull Injector injector;
 
-  /**
-   * Sends all the players within the world being unloaded to the first world in
-   * the server.
-   *
-   * @param worldId worldId  the world that is being attempted to be saved.
-   * @return if the world was able to be saved.
-   */
-  default boolean unload(final @NonNull UUID worldId) {
-    var world = Bukkit.getWorlds().get(0);
-    if (world != null) {
-      throw new IllegalStateException("Error while unloading world cannot "
-          + "find world to fallback too.");
-    }
-    return this.unload(worldId,
-        player -> player.teleportAsync(world.getSpawnLocation()));
+  private Earth() {
   }
 
   /**
-   * @param worldId  the world that is being attempted to be saved.
-   * @param onUnload called before the load is unloaded so the players can be
-   *                 transferred etc.
-   * @return if the world was able to be saved.
+   * @return Earth service that allows for control of Earth Worlds.
    */
-  boolean unload(final @NonNull UUID worldId,
-      final @NonNull Consumer<Player> onUnload);
+  public static @NonNull EarthService earthService() {
+    if (injector == null) {
+      throw new IllegalStateException("Earth has not been initialized yet.");
+    }
+    return injector.getInstance(EarthService.class);
+  }
+
 }
--- earth-api/src/main/java/io/shaded/earth/api/exception/EarthInUseException.java
+++ earth-api/src/main/java/io/shaded/earth/api/exception/EarthInUseException.java
@@ -2,8 +2,9 @@ package io.shaded.earth.api.exception;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-public abstract class EarthException extends RuntimeException {
-  public EarthException(final @NonNull String message){
+public class EarthInUseException extends EarthException {
+
+  public EarthInUseException(final @NonNull String message) {
     super(message);
   }
 }
