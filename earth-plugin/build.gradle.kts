fc2931058be20dc87876ac3239cc9c9d39315dad
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-21 16:37:47 -0600

    dep(plugin): add zstd-jni and document dependencies.

--- earth-plugin/build.gradle.kts
+++ earth-plugin/build.gradle.kts
@@ -10,7 +10,12 @@ repositories {
 dependencies {
     implementation(project(":earth-api"))
 
+    // Integration
     api("com.google.inject:guice:5.1.0")
+    // JSR 305
     compileOnlyApi("org.checkerframework:checker-qual:3.21.2")
+    // Minecraft
     compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+    // Compression
+    api("com.github.luben:zstd-jni:1.5.2-1")
 }
