951c38b9153100154da324a1d642f90e50b9d01e
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-25 11:32:35 -0600

    chore(plugin): setup Guice binding for `EarthService` and `EarthFileService`.

--- earth-plugin/src/main/java/io/shaded/earth/plugin/inject/EarthModule.java
+++ earth-plugin/src/main/java/io/shaded/earth/plugin/inject/EarthModule.java
@@ -2,11 +2,18 @@ package io.shaded.earth.plugin.inject;
 
 import com.google.inject.AbstractModule;
 import io.shaded.earth.api.Earth;
+import io.shaded.earth.api.service.EarthService;
+import io.shaded.earth.plugin.service.EarthFileService;
+import io.shaded.earth.plugin.service.internal.EarthFileServiceImpl;
+import io.shaded.earth.plugin.service.internal.EarthServiceImpl;
 
 public class EarthModule extends AbstractModule {
 
   @Override
   protected void configure() {
     this.requestStaticInjection(Earth.class);
+
+    this.bind(EarthService.class).to(EarthServiceImpl.class);
+    this.bind(EarthFileService.class).to(EarthFileServiceImpl.class);
   }
 }
