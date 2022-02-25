53167373d638d2d4a0c3092b0b447f503a1ff705
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-25 13:05:48 -0600

    chore(plugin): implement z-standard compression.

--- earth-plugin/src/main/java/io/shaded/earth/plugin/compress/ZstdCompressor.java
+++ earth-plugin/src/main/java/io/shaded/earth/plugin/compress/ZstdCompressor.java
@@ -1,5 +1,7 @@
 package io.shaded.earth.plugin.compress;
 
+import com.github.luben.zstd.Zstd;
+
 /**
  * Zstd is a compression algorithm developed by Facebook more information can be
  * found at https://facebook.github.io/zstd/. With the Java wrapper for Zstd
@@ -7,13 +9,18 @@ package io.shaded.earth.plugin.compress;
  */
 /* package-private */ final class ZstdCompressor implements EarthCompressor {
 
+  private static final int ZSTD_COMPRESSION_LEVEL = 6;
+  private static final int DECOMPRESSED_MAXIMUM_ARRAY_LENGTH = 100000000;
+
   @Override
   public byte[] compress(byte[] in) {
-    return new byte[0];
+    return Zstd.compress(in, ZSTD_COMPRESSION_LEVEL);
   }
 
   @Override
   public byte[] decompress(byte[] in) {
-    return new byte[0];
+    byte[] dst = new byte[DECOMPRESSED_MAXIMUM_ARRAY_LENGTH];
+    Zstd.decompress(dst, in);
+    return dst;
   }
 }
