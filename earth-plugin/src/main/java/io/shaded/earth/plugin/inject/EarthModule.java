51e6412964c395e3088cdc6088d12fdc76c54ba9
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-25 13:06:25 -0600

    chore(plugin): document Guice bindings and set provider for z-standard compression.

--- earth-plugin/src/main/java/io/shaded/earth/plugin/inject/EarthModule.java
+++ earth-plugin/src/main/java/io/shaded/earth/plugin/inject/EarthModule.java
@@ -1,19 +1,38 @@
 package io.shaded.earth.plugin.inject;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import io.shaded.earth.api.Earth;
 import io.shaded.earth.api.service.EarthService;
+import io.shaded.earth.plugin.compress.EarthCompressor;
+import io.shaded.earth.plugin.compress.EarthCompressor.CompressorType;
 import io.shaded.earth.plugin.service.EarthFileService;
 import io.shaded.earth.plugin.service.internal.EarthFileServiceImpl;
 import io.shaded.earth.plugin.service.internal.EarthServiceImpl;
 
 public class EarthModule extends AbstractModule {
 
+  /**
+   * Configures bindings of Guice.
+   */
   @Override
   protected void configure() {
     this.requestStaticInjection(Earth.class);
 
     this.bind(EarthService.class).to(EarthServiceImpl.class);
     this.bind(EarthFileService.class).to(EarthFileServiceImpl.class);
+  }
+
+  /**
+   * Default compressor is z-standard. With later releases other compression
+   * algorithms will be added.
+   *
+   * @return The implementation of the compressor that is used.
+   */
+  @Provides
+  @Singleton
+  public EarthCompressor compressor() {
+    return EarthCompressor.create(CompressorType.ZSTD);
   }
 }
