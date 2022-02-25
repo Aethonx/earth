5eb441795fa22d47929f1ae42063fbd003b4fb70
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-25 12:09:13 -0600

    chore(plugin): setup `Injector`

--- earth-plugin/src/main/java/io/shaded/earth/plugin/EarthPlugin.java
+++ earth-plugin/src/main/java/io/shaded/earth/plugin/EarthPlugin.java
@@ -1,11 +1,16 @@
 package io.shaded.earth.plugin;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import io.shaded.earth.plugin.inject.EarthModule;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
 public final class EarthPlugin extends JavaPlugin {
+  private @MonotonicNonNull Injector injector;
 
   @Override
   public void onEnable() {
-
+    this.injector = Guice.createInjector(new EarthModule());
   }
 }
