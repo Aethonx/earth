b5a6d844126563cce8bdc46560463cd8ac534f97
Bruce <45923326+Aethonx@users.noreply.github.com>
2022-02-26 11:10:11 -0600

    chore(plugin): paper compileOnly -> compileOnlyApi

--- earth-plugin/build.gradle.kts
+++ earth-plugin/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     // JSR 305
     compileOnlyApi("org.checkerframework:checker-qual:3.21.2")
     // Minecraft
-    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+    compileOnlyApi("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
     // Compression
     api("com.github.luben:zstd-jni:1.5.2-1")
 }
